601f29151a32e5a91db752d1cd24c2ecc23647d1
iamlohit <jethwanilohit@gmail.com>
2024-05-12 21:47:30 +0530

    Built a basic webserver

---
+++ user-management/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	// Setup a route
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	// Start the server on port 8080
+	router.Run(":8080")
+}
