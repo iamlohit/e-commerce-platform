89b88d65c95529d4378f5c7017521280a96f2665
iamlohit <jethwanilohit@gmail.com>
2024-05-12 21:57:47 +0530

    Built a Route for user Registration

--- user-management/main.go
+++ user-management/main.go
@@ -9,11 +9,17 @@ import (
 func main() {
 	router := gin.Default()
 
-	// Setup a route
+	// Route for 'ping' to check if the service is up
 	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+
+		// Route for user registration
+		router.POST("/register", func(c *gin.Context) {
+			// For now we are simulating user registration
+			// Later we will extract the data from the c.Request.Body and save it to a database we create
+			c.JSON(http.StatusOK, gin.H{"message": "registration successful"})
 		})
+
 	})
 	// Start the server on port 8080
 	router.Run(":8080")
